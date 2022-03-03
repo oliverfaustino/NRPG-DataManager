1cbddfba52f0201bb07cd1e1e0d0f4a16461fa7b
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-03-02 22:51:09 -0300

    Local para armazenar as informações legais do sistema

---
+++ modulos/info.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+# informações do programa
+name = 'NRPG-Datamanager'
+version = 5.1
+date_version = '01-03-2022'
+objective = """Um programa para manipular as informações do banco de dados do "N'RPG-Revolution": um RPG Textual de Whatsapp"""
+owner = 'Cr4sh-RV (Oliver da Silva Faustino)'
+contributors = []
+
+
+# hora de execução do programa
+tempo = datetime.now()
