6622092ea2c2404094a07eeab46f0ba9e8d2402f
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-03-08 22:42:12 -0300

    Select que me possibilita ver a patente de ninjas de uma determinada afiliação

---
+++ sql/insert_patente_afiliacao.sql
@@ -0,0 +1,7 @@
+﻿SELECT p.nome as dono_do_pp, p.numero, pp.nome as nome_do_pp, a.nome as afiliação_do_pp,pa.nome as patente_do_pp
+	FROM player p, pp, patente pa, afiliacao a
+	where pp.id_player = p.id_player 
+	and pp.id_patente = pa.id_patente
+	and pp.id_afiliacao = a.id_afiliacao
+	and pp.id_afiliacao = 1
+	and pp.id_patente = 3;
