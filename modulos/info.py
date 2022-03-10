e8815912bf9c7a0e1e2325e88d31afbb79e5fa6b
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-03-09 22:08:12 -0300

    Adicionado à versão 6.3, na qual roda em torno de ajustes de melhorias da prática do sistema

--- modulos/info.py
+++ modulos/info.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 # informações do programa
 name = 'NRPG-Datamanager'
-version = 6.2
+version = 6.3
 date_version = '01-03-2022'
 objective = """Um programa para manipular as informações do banco de dados do "N'RPG-Revolution": um RPG Textual de Whatsapp"""
 owner = 'Cr4sh-RV (Oliver da Silva Faustino)'
