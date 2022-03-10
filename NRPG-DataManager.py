408bf5cceb05166b2b9ac8c7ffa2fbdff455d5f9
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-03-09 22:09:16 -0300

    Mudado o tempo da splash screen para 2 segundos

--- NRPG-DataManager.py
+++ NRPG-DataManager.py
@@ -2,7 +2,7 @@ from modulos.query import *
 from modulos.splash_screen import *
 
 if __name__ == '__main__':
-    splash_screen(segundos = 6)
+    splash_screen(segundos = 2)
 
     while True:
         query()
