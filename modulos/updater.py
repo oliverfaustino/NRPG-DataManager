77c22d1f59b66c8b1d12521f890ebfee67303a6b
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-04-17 02:21:09 -0300

    Removido a função de adicionar check in, pois ela deveria estar nos adder, não nos updater

--- modulos/updater.py
+++ modulos/updater.py
@@ -38,36 +38,4 @@ def update_base(nome, limite):
             base = f'『❌』> {nome} (LIMITE ULTRAPASSADO!!! /{limite})'
         else:
             base = f'『❌』> {nome} ({conta}/{limite})'        
-        return base
-
-
-
-# atualizar check_in
-def update_check_in():
-    select('select id_player, nome, check_in from player order by id_player asc')
-    confirmacao = False
-    while confirmacao == False:
-        
-        try:
-            player = int(input('Digite o Id do player: '))
-            check_in = float(input('Digite o novo valor: '))
-            sql = f'UPDATE player SET check_in = {check_in} WHERE id_player = {player}'
-            confirmacao_1 = str(input(f'''Confirmação dos valores:
-    
-    {sql} 
-
-    está correto? 1 para "sim" e qualquer valor para "não". '''))
-            
-            if confirmacao_1 == '1':
-                confirmacao = True
-            
-                try:
-                    pd.read_sql_query(sql, con=engine)
-            
-                except:
-                    pass
-        
-        except ValueError:
-            print('\nO valor não corresponde. Tente novamente')
-
-    return
+        return base   
