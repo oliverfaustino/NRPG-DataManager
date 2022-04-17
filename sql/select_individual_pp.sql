aaf4799abb332b6fa6a79ea52fa238eef5671fa2
oliverfaustino <oliverdasilvafaustino@gmail.com>
2022-04-17 02:14:18 -0300

    Query que consegue trazer todas as informações de um personagem corretamente

--- sql/select_individual_pp.sql
+++ sql/select_individual_pp.sql
@@ -1,104 +1,57 @@
 ﻿SELECT pp.id_pp, pp.id_player, pp.nome, pp.aparencia, pp.sangue, pp.base, pp.cla_1, pp.cla_2, 
+       A.nome as afiliação, PA.nome as patente, CA.nome as cargo, E1.nome as elemento_1, 
+       E2.nome as e_2, E3.nome as e_3, E4.nome ase_4, E5.nome as e_5, pp.data_criacao, PR1.nome as prêmio_guardado_1, 
+       PR2.nome as prêmio_2, PR3.nome as prêmio_3, PR4.nome as prêmio_4, dinheiro, H1.nome as hiden_1, 
+       H2.nome as hiden_2, H3.nome as hiden_3, J.nome as juinjutsu, K1.nome as Kinjutsu_1, K2.nome as kinjutsu_2, 
+       K3.nome as kinjutsu_3, K4.nome as kinjutsu_4, K5.nome as kinjutsu_5, KKG1.nome as kkg_1, KKG2.nome as kkg_2, 
+       KKG3.nome as kkg_3, KKG4.nome as kkg_4, KKG5.nome as kkg_5, pp.modificacao_corporal, S1.nome as senjutsu_1, 
+       S2.nome as senjutsu_2, S3.nome as senjutsu_3, TU1.nome as traço_único_1, TU2.nome as traço_único_2, 
+        TU3.nome as traço_único_3, Y1.nome as yugojutsu_1, Y2.nome as yugojutsu_2, Y3.nome as yugojutsu_3
+  FROM pp
 
-       a.id_afiliacao,
+inner join afiliacao A on pp.id_afiliacao = A.id_afiliacao
+inner join patente PA on pp.id_patente = PA.id_patente
+inner join cargo CA on pp.id_cargo = CA.id_cargo
 
-       pa.id_patente,
+inner join elemento E1 on pp.id_elemento_1 = E1.id_elemento 
+inner join elemento E2 on pp.id_elemento_2 = E2.id_elemento 
+inner join elemento E3 on pp.id_elemento_3 = E3.id_elemento 
+inner join elemento E4 on pp.id_elemento_4 = E4.id_elemento 
+inner join elemento E5 on pp.id_elemento_5 = E5.id_elemento
 
-       c.id_cargo,
+inner join premio PR1 on pp.id_premio_1 = PR1.id_premio
+inner join premio PR2 on pp.id_premio_2 = PR2.id_premio
+inner join premio PR3 on pp.id_premio_3 = PR3.id_premio
+inner join premio PR4 on pp.id_premio_4 = PR4.id_premio
 
-       e.nome,
+inner join hiden H1 on pp.id_hiden_1 = H1.id_hiden
+inner join hiden H2 on pp.id_hiden_2 = H2.id_hiden
+inner join hiden H3 on pp.id_hiden_3 = H3.id_hiden
 
-       pp.registro_ninja, pp.data_criacao, 
+inner join juinjutsu J on pp.id_juinjutsu = J.id_juinjutsu
 
-       pr.nome,
+inner join kinjutsu K1 on pp.id_kinjutsu_1 = K1.id_kinjutsu
+inner join kinjutsu K2 on pp.id_kinjutsu_2 = K2.id_kinjutsu
+inner join kinjutsu K3 on pp.id_kinjutsu_3 = K3.id_kinjutsu
+inner join kinjutsu K4 on pp.id_kinjutsu_4 = K4.id_kinjutsu
+inner join kinjutsu K5 on pp.id_kinjutsu_5 = K5.id_kinjutsu
 
-       pp.dinheiro,
+inner join kkg KKG1 on pp.id_kkg_1 = KKG1.id_kkg
+inner join kkg KKG2 on pp.id_kkg_2 = KKG2.id_kkg
+inner join kkg KKG3 on pp.id_kkg_3 = KKG3.id_kkg
+inner join kkg KKG4 on pp.id_kkg_4 = KKG4.id_kkg
+inner join kkg KKG5 on pp.id_kkg_5 = KKG5.id_kkg
 
-       h.nome,
+inner join senjutsu S1 on pp.id_senjutsu_1 = S1.id_senjutsu
+inner join senjutsu S2 on pp.id_senjutsu_2 = S2.id_senjutsu
+inner join senjutsu S3 on pp.id_senjutsu_3 = S3.id_senjutsu
 
-       j.id_juinjutsu,
+inner join traco_unico TU1 on pp.id_traco_unico_1 = TU1.id_traco_unico
+inner join traco_unico TU2 on pp.id_traco_unico_2 = TU2.id_traco_unico
+inner join traco_unico TU3 on pp.id_traco_unico_3 = TU3.id_traco_unico
 
-       k.nome,
+inner join yugojutsu Y1 on pp.id_yugojutsu_1 = Y1.id_yugojutsu
+inner join yugojutsu Y2 on pp.id_yugojutsu_2 = Y2.id_yugojutsu
+inner join yugojutsu Y3 on pp.id_yugojutsu_3 = Y3.id_yugojutsu 
 
-       kkg.nome,
-
-       pp.modificacao_corporal, 
-
-       s.nome,
-
-       t.nome, 
-
-       y.nome
-       
-  FROM pp, afiliacao a, patente pa, cargo c, elemento e, premio pr, hiden h, juinjutsu j, kinjutsu k, kkg, senjutsu s, traco_unico t, yugojutsu y
-
-  where 
-a.id_afiliacao = pp.id_afiliacao
-and
-       pa.id_patente = pp. id_patente
-and
-       c.id_cargo = pp.id_cargo
-and
-       e.id_elemento = pp.id_elemento_1
-and
-       e.id_elemento = pp.id_elemento_2
-and
-       e.id_elemento = pp.id_elemento_3
-and
-       e.id_elemento = pp.id_elemento_4 
-and
-       pr.id_premio = pp.id_premio_1
-and
-       pr.id_premio = pp.id_premio_2
-and
-       pr.id_premio = pp.id_premio_3
-and
-       pr.id_premio = pp.id_premio_4 
-and
-       h.id_hiden = pp.id_hiden_1
-and
-       h.id_hiden = pp.id_hiden_2 
-and
-       h.id_hiden = pp.id_hiden_3
-and
-       j.id_juinjutsu = pp.id_juinjutsu
-and
-       k.id_kinjutsu = pp.id_kinjutsu_1
-and
-       k.id_kinjutsu = pp.id_kinjutsu_2
-and
-       k.id_kinjutsu = pp.id_kinjutsu_3
-and
-       k.id_kinjutsu = pp.id_kinjutsu_4
-and
-       k.id_kinjutsu = pp.id_kinjutsu_5
-and
-       kkg.id_kkg = pp.id_kkg_1
-and
-       kkg.id_kkg = pp.id_kkg_2
-and
-       kkg.id_kkg = pp.id_kkg_3
-and
-       kkg.id_kkg = pp.id_kkg_4
-and
-       kkg.id_kkg = pp.id_kkg_5
-and
-       s.id_senjutsu = pp.id_senjutsu_1
-and
-       s.id_senjutsu = pp.id_senjutsu_2 
-and
-       s.id_senjutsu = pp.id_senjutsu_3
-and
-       t.id_traco_unico = pp.id_traco_unico_1
-and
-       t.id_traco_unico = pp.id_traco_unico_2
-and
-       t.id_traco_unico = pp.id_traco_unico_3 
-and
-       y.id_yugojutsu = pp.id_yugojutsu_1
-and
-       y.id_yugojutsu = pp.id_yugojutsu_2
-and
-       y.id_yugojutsu = pp.id_yugojutsu_3
-and
-	pp.id_pp = 1.1
+WHERE id_pp = 1.1
